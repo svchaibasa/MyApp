eaabb10512a6e1b0574fb743c46bf552183b17de
svchaibasa <42116004+svchaibasa@users.noreply.github.com>
2018-08-15 17:24:09 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
-  mongoURI:'mongodb://localhost:27017/PrackrApp',
+    //mongoURI:'mongodb://localhost:27017/PrackrApp',
+  mongoURI: 'mongodb://sumit:sumit123@ds123012.mlab.com:23012/prackrapp',
   secretOrKey: 'secret'
 };
